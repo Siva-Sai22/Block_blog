e8e6b346118352e1724ddebdaa299bdb3dc25784
Siva_Sai <22je0483@iitism.ac.in>
2024-01-27 17:35:30 +0530

    Implement react router and a backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Homepage from './pages/Homepage.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage/>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import Cards from "../components/Cards";
+import "../index.css";
+
+function Homepage() {
+  return (
+    <>
+      <Header></Header>
+      <Cards></Cards>
+    </>
+  );
+}
+
+export default Homepage;
